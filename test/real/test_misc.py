900d42cfe79535ceb118b302a26282ac94167eac
Vinícius Costa <viniciusccosta95@gmail.com>
2023-05-15 18:02:08 -0300

    Inclusão de testes de CFOPs.

--- test/real/test_misc.py
+++ test/real/test_misc.py
@@ -1,14 +1,25 @@
 import unittest
 from pysisnoapi import misc
+from test.misc_common import *
 
-class PySisnoApiTest(unittest.TestCase):
+class MiscTest(unittest.TestCase):
 
     def test_consulta_municipio(self):
         uf = "DF"
 
         data = misc.get_municipios(uf=uf)
         
-        assert data == [{'codigo_ibge': 5300108, 'descricao': 'Brasilia'}]
+        check_municipios_do_DF(self, data)
+
+    def test_consulta_cfops_length(self):
+        data = misc.get_cfops()
+        
+        check_cfops_length(self, data)
+
+    def test_consulta_cfops_atributos(self):
+        data = misc.get_cfops()
+        
+        check_cfop_attributes(self, data)
 
 if __name__ == "__main__":
     unittest.main()
