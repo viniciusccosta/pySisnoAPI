8f79519e90c2c8ee53de4bd4b61dbfad9ba56c65
Vinícius Costa <viniciusccosta95@gmail.com>
2023-05-15 18:01:40 -0300

    Inclusão de módulos com funções em comum entre os testes simulados e os testes reais.

---
+++ test/misc_common.py
@@ -0,0 +1,14 @@
+def check_municipios_do_DF(test_case, data):
+    test_case.assertListEqual(data, [{'codigo_ibge': 5300108, 'descricao': 'Brasilia'}] )
+
+def check_cfops_length(test_case, data):
+    test_case.assertGreater(len(data), 0)
+
+def check_cfop_attributes(test_case, data):
+    atributos = ["codigo", "descricao", "aplicacao"]
+    cfop = data[0]
+    for atributo in atributos:
+        with test_case.subTest(key=atributo):
+            test_case.assertIn(atributo, cfop)
+
+# TODO: Check se informado um UF inválido na consulta de municípios
---
+++ test/pysisnoapi_common.py
@@ -0,0 +1,5 @@
+# TODO: Teste para verificar se as chaves da API estão configuradas
+# TODO: Teste para verificar se as chaves são válidas
+# TODO: Teste para verificar se a plataforma reconhece as chaves
+# TODO: Teste para verificar se existe .env
+# TODO: Teste para verificar se a função de alterar as chaves da API está funcionando
