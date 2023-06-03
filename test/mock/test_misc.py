54185aa523b4e57e7efd1e6e28d80e2e42da1750
Vinícius Costa <viniciusccosta95@gmail.com>
2023-06-03 12:30:25 -0300

    Testes mockados para função "get_municipios"

--- test/mock/test_misc.py
+++ test/mock/test_misc.py
@@ -1,23 +1,74 @@
-import requests_mock
+# ==========================================================================
 import unittest
+import requests
+
 from pysisnoapi import misc
 from test.misc_common import *
+from unittest.mock import MagicMock
+from typing import List
 
+# ==========================================================================
+UF = 'MG'
+
+# ==========================================================================
 class MiscTest(unittest.TestCase):
+    def test_get_municipios_success(self):
+        # Mocking:
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            "status": "Sucesso",
+            "descricao": "Lista de municípios",
+            'dados': [
+                {
+                    'codigo_ibge': 3106200,
+                    'descricao': 'Belo Horizonte',
+                },
+                {
+                    'codigo_ibge': 3118601,
+                    'descricao': 'Contagem',
+                },
+            ]
+        }
+        requests.get = MagicMock(return_value=mock_response)
 
-    def test_consulta_municipios_do_DF(self):
-        uf = "DF"
+        # Chama a função:
+        municipios = misc.get_municipios(UF)
 
-        with requests_mock.Mocker() as mocker:
-            mocker.register_uri(
-                requests_mock.ANY,
-                requests_mock.ANY,
-                json={'dados': [{'codigo_ibge': 5300108, 'descricao': 'Brasilia'}]}
-            )
-
-            data = misc.get_municipios(uf=uf)
+        # Checando resultado:
+        self.assertIsInstance(municipios, List)
+        self.assertEqual(len(municipios), 2)
+        self.assertIsInstance(municipios[0], Municipio)
+        self.assertEqual(municipios[0].descricao, 'Belo Horizonte')
+        self.assertEqual(municipios[1].codigo_ibge, 3118601)
         
-        check_municipios_do_DF(self, data)
+    def test_get_municipios_falha_412(self):
+        # Nocking:
+        mock_response = MagicMock()
+        mock_response.status_code = 412
+        requests.get = MagicMock(return_value=mock_response)
 
+        # Chama a função:
+        municipios = misc.get_municipios(UF)
+
+        # Checando resultado:
+        self.assertIsInstance(municipios, List)
+        self.assertEqual(len(municipios), 0)
+
+    def test_get_municipios_falha_outros(self):
+        # Nocking:
+        mock_response = MagicMock()
+        mock_response.status_code = 500
+        requests.get = MagicMock(return_value=mock_response)
+
+        # Chama a função:
+        municipios = misc.get_municipios(UF)
+
+        # Checando resultado:
+        assert municipios is None
+
+# ==========================================================================
 if __name__ == "__main__":
     unittest.main()
+    
+# ==========================================================================
