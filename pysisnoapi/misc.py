99227f0bf5c74ad8f1803aada8253572e6d7253f
viniciusccosta <viniciusccosta95@gmail.com>
2023-10-28 17:14:54 -0300

    Ordenação alfabética em misc

--- pysisnoapi/misc.py
+++ pysisnoapi/misc.py
@@ -27,45 +27,6 @@ from . import (
 
 # ======================================================================================================================
 @validate_call
-async def get_municipios(token_emissor: str,
-                   token_secret_emissor: str,
-                   uf: str,
-                   *args, **kwargs) -> List[Municipio]:
-    '''Consulta os municípios de um determinado estado através de uma requisição à API.
-
-    Essa função permite consultar os municípios de um estado específico através de uma requisição à API.
-    Ela retorna uma lista de objetos Municipio contendo informações sobre cada município.
-
-    Args:
-        uf (str): UF do estado para o qual deseja-se consultar os municípios.
-
-    Returns:
-        List[Municipio]: Uma lista de objetos Municipio, representando os municípios do estado consultado.
-    '''
-
-    headers  = HEADERS.copy()
-
-    validate_tokens(token_emissor, token_secret_emissor)
-    headers['token-emissor']        = token_emissor
-    headers['token-secret-emissor'] = token_secret_emissor
-
-    url = f'{BASE_URL}/unidades-federativas/{uf}/municipios'
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=headers)
-
-    match (response.status_code):
-        case 200:
-            json_data  = response.json().get('dados')
-            municipios = [Municipio(**d) for d in json_data]
-            return municipios
-        case 412:
-            return []
-        case _:
-            return
-
-    # TODO: return response ?
-
-@validate_call
 async def get_cfops(token_emissor: str,
               token_secret_emissor: str,
               *args, **kwargs) -> List[Cfop]:
@@ -142,6 +103,45 @@ async def get_ibpts(token_emissor: str,
             json_data = response.json().get('dados')
             ibpts     = [Ibpt.from_json(**d) for d in json_data]
             return ibpts
+        case 412:
+            return []
+        case _:
+            return
+
+    # TODO: return response ?
+
+@validate_call
+async def get_municipios(token_emissor: str,
+                   token_secret_emissor: str,
+                   uf: str,
+                   *args, **kwargs) -> List[Municipio]:
+    '''Consulta os municípios de um determinado estado através de uma requisição à API.
+
+    Essa função permite consultar os municípios de um estado específico através de uma requisição à API.
+    Ela retorna uma lista de objetos Municipio contendo informações sobre cada município.
+
+    Args:
+        uf (str): UF do estado para o qual deseja-se consultar os municípios.
+
+    Returns:
+        List[Municipio]: Uma lista de objetos Municipio, representando os municípios do estado consultado.
+    '''
+
+    headers  = HEADERS.copy()
+
+    validate_tokens(token_emissor, token_secret_emissor)
+    headers['token-emissor']        = token_emissor
+    headers['token-secret-emissor'] = token_secret_emissor
+
+    url = f'{BASE_URL}/unidades-federativas/{uf}/municipios'
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+
+    match (response.status_code):
+        case 200:
+            json_data  = response.json().get('dados')
+            municipios = [Municipio(**d) for d in json_data]
+            return municipios
         case 412:
             return []
         case _:
